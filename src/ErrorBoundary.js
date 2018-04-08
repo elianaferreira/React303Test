1330424d03bc5eda63e79aede2ae80a836144831
eferreira <elianaef817@gmail.com>
2018-04-08 01:03:35 -0400

    added error boundary, use of App.css for style

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+class ErrorBoundary extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { error: null }
+  }
+
+  componentDidCatch (error) {
+    this.setState({error: error})
+  }
+
+  render () {
+    if (this.state.error) {
+      return (
+        <div className='alert'>
+          <strong>Error: </strong> An error has occurred loading the page.
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+export default ErrorBoundary
