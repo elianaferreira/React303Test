e1f3a8a2580c7cb65728a4019b75d4482dd74b87
eferreira <elianaef817@gmail.com>
2018-04-08 01:24:41 -0400

    added more data to post view

--- src/Post.js
+++ src/Post.js
@@ -19,6 +19,10 @@ const Post = props => {
   return (
     <React.Fragment>
       <h2 className='header'>{props.history.location.state.post.title}</h2>
+      <div className='subheader'>
+        <span className='author'>by {props.history.location.state.post.authors}</span>
+        <span className='date'>at {new Date(props.history.location.state.post.date).toDateString()}</span>
+      </div>
       <img src={props.history.location.state.post.featured_media.url} width={'40%'}/>
       <section className='content'>
         {props.history.location.state.post.content.map((p, index) => {
@@ -27,6 +31,11 @@ const Post = props => {
           } else if (p.type === 'block_quote') {
             return <BlockQuote key={index} text={p.text} />
           }
+        })}
+      </section>
+      <section className='tagBox'>
+        {props.history.location.state.post.tags.map(tag => {
+          return <span className='tag'>{tag.name}</span>
         })}
       </section>
     </React.Fragment>
